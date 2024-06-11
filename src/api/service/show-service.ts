8cb38976438149b0325d862c7275f5aeee63945c
Ionela Cristea <oanacristea.1007@yahoo.com>
2024-06-11 22:49:21 +0200

    implemented: data to the home page, optimization wip, search, style

--- src/api/service/show-service.ts
+++ src/api/service/show-service.ts
@@ -1,21 +1,104 @@
 import {ShowRepository} from "@/api/repository/show-repository";
-import type {ShowInfo, ShowModel} from "@/models/show-model";
-import { db } from "@/database/indexDB"
+import type {ShowInfo, ShowModel, ShowsByGenreModel} from "@/models/show-model";
+import {insertInSortedOrder} from "@/utilities/show-info";
 
 export class ShowService {
     private showRepository: ShowRepository;
 
     constructor() {
         this.showRepository = new ShowRepository();
+    }
+
+
+    public async getShowInfoPerPage(page: number): Promise<ShowInfo[] | null> {
+        const response = await this.showRepository.fetchShows(page);
+
+        if (response !== null) {
+            return response.map((show: ShowModel) => {
+                return {
+                    id: show.id,
+                    name: show.name,
+                    type: show.type,
+                    genres: show.genres,
+                    image: show.image,
+                    rating: show.rating
+                };
+            });
+        }
+
+        return null;
+    }
+
+    /**
+     * Get all data and keep it in memory and use it immediately since is faster
+     */
+    public async getRecursivelyShowsInfo() {
+        const a = performance.now();
+        console.log('START...');
+
+        let page = 0;
+        let showsInfo: ShowInfo[] = [];
+        let response = await this.getShowInfoPerPage(page);
+
+        if (response === null) {
+            return null;
+        }
+
+        // todo: process here the grouping by genre
+        while (response !== null && page < 10) {
+            showsInfo = showsInfo.concat(response);
+            page++;
+            response = await this.getShowInfoPerPage(page);
+        }
+
+
+        // end of test
+        const b = performance.now();
+        console.log('FINISH in ' + (b - a) + ' ms.');
+
+        return showsInfo;
+    }
+
+
+    /**
+     * Initially use sorted data from memory
+     */
+    public async getShowsInfoFromMemoryGroupedByGenre(): Promise<ShowsByGenreModel| null> {
+          const showsInfo = await this.getRecursivelyShowsInfo();
+
+            if (showsInfo === null) {
+                return null;
+            }
+
+            const showsByGenre: { [genres: string]: ShowInfo[] } = {};
+
+            showsInfo.forEach((show: ShowInfo) => {
+                show.genres.forEach((genre: string) => {
+                    if (!showsByGenre[genre]) {
+                        showsByGenre[genre] = [];
+                    }
+                    insertInSortedOrder(showsByGenre[genre], show);
+                    // showsByGenre[genre].push(show);
+                });
+            });
+
+            return showsByGenre;
 
     }
 
     // todo: optimise this function
     public async fetchRecursivelyAndProcess() {
+        const isDbDataInsertedToday = await this.showRepository.isDbDataInsertedToday();
+
+        // Do nothing if data is inserted today
+        if (isDbDataInsertedToday) {
+            return;
+        }
+
         let page = 0;
         let response = await this.setShowsInfo(page);
 
-        //  to test the performance
+        // to test the performance
         const a = performance.now();
         console.log('start...');
 
@@ -26,7 +109,6 @@ export class ShowService {
         }
 
         // end of test
-        await this.showRepository.checkAndFetchDataIfNeeded();
         const b = performance.now();
         console.log('Finished in ' + (b - a) + ' ms.');
     }
@@ -45,6 +127,7 @@ export class ShowService {
                 type: show.type,
                 genres: show.genres,
                 rating: show.rating,
+                image: show.image,
                 createdAt: new Date()
             };
         });
@@ -55,5 +138,14 @@ export class ShowService {
 
     }
 
+
+    // todo: consider storing data from memory to indexedDB
+    // but it might lost if the user refreshes the page
+    // or closes the browser
+
+
+    public async getShowsInfoFromDb() {
+        return await this.showRepository.getShowsGroupedByGenre();
+    }
 
 }
